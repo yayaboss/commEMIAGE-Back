59f19a5792bfb4671fe07f03b3a72b8fa20e5b40
FakihD <fakih.darkaoui@hotmail.com>
2019-02-04 17:13:38 +0100

    Ajout des models Semestre, Suivi, Tuteur

--- models/Tuteur.js
+++ models/Tuteur.js
@@ -3,7 +3,7 @@ const mongoose 	= require('mongoose'),
     Schema	 	= mongoose.Schema;
 
 //------------------------------------------- Resources Schema
-let ApprenantSchema = new Schema({
+let TuteurSchema = new Schema({
     nom : {type: String, stringTransform: function(string) {
         return string.toUpperCase();
     }},
@@ -11,22 +11,14 @@ let ApprenantSchema = new Schema({
     adresse : String,
     email : {type: String, unique: true, regex: new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")},
     dateFormation : Date,
-    filiere : {type: Object, objectType: {
-        nom: String
-    }},
-    semestre : [{
-            nom : String
-    }],
     module : [{
             nom : String
     }],
-    evaluation : [{
-            alias : String,
-    }],
-    suivi : [{
-            alias : String,
+    apprenant : [{
+        nom : String,
+        prenom : String
     }]
 });
 
-let Apprenant = mongoose.model('Apprenant', ApprenantSchema);
-module.exports = Apprenant;
+let Tuteur = mongoose.model('Tuteur', TuteurSchema);
+module.exports = Tuteur;
